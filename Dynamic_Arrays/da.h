dce8392b8cc2cf03a5a4e8ded7c16507a4cdf29e
Jacob <jacobkriss2001@gmail.com>
2024-11-08 13:23:49 -0500

    dynamic arrays started

---
+++ Dynamic_Arrays/da.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+// Dynamic strictly typed arrays 
+
+typedef union{
+    int type_int;
+    char type_char; 
+    float type_float;
+    char * type_string; 
+}data_type; 
+
+typedef struct{
+    int length; 
+    char * type; 
+    data_type *array;  
+}d_array; 
+
+// Create a new array of specified starting length and type
+d_array create_array(int length, char * type); 
+
+// Add data to end of array 
+void append(d_array array, data_type data);
+
+/*pop should remove last index value if both passed index params are null, 
+otherwise remove and return value at specified index*/
+data_type pop(d_array array, int index);
+
+//Cut will take two indices (inclusive) and cut out all data at and between the specified indices 
+void cut(d_array array, int start_index, int end_index);
+
+//sort array 
+// pass in reversed as only param if sort should be done in reverse order 
+// on any array 30 elements or less I will use insertion sort
+// on any array with size greater than 30 and less than 1,000 I will use heap sort
+// on any array with size greater than 1,000 I will use merge sort (insertion sort when the broken down arrays reach size less than 30)
+void sort(d_array array); 
+
+
--- Linked_List/ll.h
+++ Linked_List/ll.h
@@ -1,1 +1,20 @@
-#include 
+#include <stdio.h> 
+
+// Lets make a union which we will use as the data type 
+// for now I will just allow ints and chars
+// Once I implement a dynamic array datatype I will use that to allow for strings
+typedef union{
+    int *int_data; 
+    char *char_data; 
+}Data;
+
+typedef struct{
+    Data data; //Don't use * because it is an inferred pointer due to union definition
+    Node *next; //has attribute of type node which is a pointer to a Node type 
+}Node;
+
+typedef struct{
+    Node *head; 
+}LL; 
+
+LL create_ll(Data head_data);
